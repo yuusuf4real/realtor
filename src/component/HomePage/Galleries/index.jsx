65941d33ecab1e9f229bff4c8f7706f55984abe6
yusuf <yuusuf4real@yahoo.com>
2021-06-17 22:45:54 +0100

    listing-created

--- src/component/HomePage/Galleries/index.jsx
+++ src/component/HomePage/Galleries/index.jsx
@@ -1,13 +1,54 @@
 import React from 'react';
 
-import { StyledGall, StyledHeading, StyledListing1 } from './style'
+import { StyledGall, StyledHeading, StyledListing1, StyledListing2, StyledListing3, 
+        StyledListingImg1, StyledListingTxt1, StyledListingImg2, StyledListingTxt2, 
+        StyledListingImg3, StyledListingTxt3, StyledGallImg1, StyledGallImg2, 
+        StyledGallImg3, StyledGallPrice1, StyledGallAddr1, StyledGallDes1, 
+        StyledGallRealtor1, StyledGallPrice2, StyledGallAddr2, StyledGallDes2, 
+        StyledGallRealtor2, StyledGallPrice3, StyledGallAddr3, StyledGallDes3, StyledGallRealtor3 } from './style'
+import listing1 from '../../../assets/listing1.jpg';
+import listing2 from '../../../assets/listing2.jpg';
+import listing3 from '../../../assets/listing3.jpg';
 
 const Galleries = () => {
     return (
         <StyledGall>
             <StyledHeading>Recent Listings</StyledHeading>
-            <StyledListing1></StyledListing1>
-           
+            <StyledListing1>
+                <StyledListingImg1>
+                    <StyledGallImg1 src={listing1} />
+                </StyledListingImg1>
+                <StyledListingTxt1>
+                    <StyledGallPrice1>#150,000,000</StyledGallPrice1>
+                    <StyledGallAddr1>PackView Estate, Chevron Drive, Lekki Phase1</StyledGallAddr1>
+                    <StyledGallDes1>3 Bedsrooms&nbsp;&nbsp; 2 Baths</StyledGallDes1><br />
+                    <StyledGallRealtor1>Kola Real Ventures</StyledGallRealtor1>
+                </StyledListingTxt1>
+            </StyledListing1>
+
+            <StyledListing2>
+                <StyledListingImg2>
+                    <StyledGallImg2 src={listing2} />
+                </StyledListingImg2>
+                <StyledListingTxt2>
+                <StyledGallPrice2>#220,000,000</StyledGallPrice2>
+                    <StyledGallAddr2>Ariyib Crescent, Admiralty Way, Lekki Phase1</StyledGallAddr2>
+                    <StyledGallDes2>3 Bedsrooms&nbsp;&nbsp; 2 Baths</StyledGallDes2><br />
+                    <StyledGallRealtor2>Adron Homes</StyledGallRealtor2>
+                </StyledListingTxt2>
+            </StyledListing2>
+
+            <StyledListing3>
+                <StyledListingImg3>
+                    <StyledGallImg3 src={listing3} />
+                </StyledListingImg3>
+                <StyledListingTxt3>
+                    <StyledGallPrice3>#250,000,000</StyledGallPrice3>
+                    <StyledGallAddr3>Orseborne Drive, Orseborne Estate, Ikoyi, Lagod</StyledGallAddr3>
+                    <StyledGallDes3>3 Bedsrooms&nbsp;&nbsp; 2 Baths</StyledGallDes3><br />
+                    <StyledGallRealtor3>Homes And Hormses</StyledGallRealtor3>
+                </StyledListingTxt3>
+            </StyledListing3>
         </StyledGall>
     )
 }
--- src/component/HomePage/Galleries/style.js
+++ src/component/HomePage/Galleries/style.js
@@ -2,9 +2,8 @@ import styled from "styled-components";
 
 export const StyledGall = styled.div`
     width: 100%;
-    height: 33em;
+    height: 36em;
     border: 1px solid;
-    background-color: turquoise;
 `;
 
 export const StyledHeading = styled.h1`
@@ -13,11 +12,170 @@ export const StyledHeading = styled.h1`
     margin-top: 1em;
 `;
 
+
 export const StyledListing1 = styled.div`
-    width: 19em;
-    height: 27em;
-    border: 1px solid;
-    border-radius: 1em;
-    margin-left: 7em;
+    width: 20em;
+    height: 29em;
+    float: left;
+    margin-left: 9em;
+    margin-top: 1em;
+    background-color: #DCDCDC;
+    box-shadow: 5px 10px #888888;
+`;
+
+
+export const StyledListing2 = styled.div`
+    width: 20em;
+    height: 29em;
+    float: left;
+    margin-left: 3em;
+    margin-top: 1em;
+    background-color: #DCDCDC;
+    box-shadow: 5px 10px #888888;
+`;
+
+export const StyledListing3 = styled.div`
+    width: 20em;
+    height: 29em;
+    float: left;
+    margin-left: 3em;
+    margin-top: 1em;
+    background-color: #DCDCDC;
+    box-shadow: 5px 10px #888888;
+`;
+
+export const StyledListingImg1 = styled.div`
+    width: 16.8em;
+    height: 13em;
+    margin-left: 1.5em;
     margin-top: 1em;
 `;
+
+export const StyledListingTxt1 = styled.div`
+    width: 16.8em;
+    height: 13.7em;
+    margin-left: 1.5em;
+    margin-top: 1em;
+`;
+
+export const StyledListingImg2 = styled.div`
+    width: 16.8em;
+    height: 13em;
+    margin-left: 1.5em;
+    margin-top: 1em;
+`;
+
+export const StyledListingTxt2 = styled.div`
+    width: 16.8em;
+    height: 13.7em;
+    margin-left: 1.5em;
+    margin-top: 1em;
+`;
+
+export const StyledListingImg3 = styled.div`
+    width: 16.8em;
+    height: 13em;
+    margin-left: 1.5em;
+    margin-top: 1em;
+`;
+
+export const StyledListingTxt3 = styled.div`
+    width: 16.8em;
+    height: 13.7em;
+    margin-left: 1.5em;
+    margin-top: 1em;
+`;
+
+export const StyledGallImg1 = styled.img`
+    width: 16.8em;
+    height: 13em;
+`;
+
+export const StyledGallImg2 = styled.img`
+    width: 16.8em;
+    height: 13em;
+`;
+
+export const StyledGallImg3 = styled.img`
+    width: 16.8em;
+    height: 13em;
+`;
+
+export const StyledGallPrice1 = styled.h1`
+    font-weight: 500;
+    float: left;
+`;
+
+export const StyledGallAddr1 = styled.p`
+    font-size: 1.1rem;
+    float: left;
+    margin-top: -0.5em;
+`;
+
+export const StyledGallDes1 = styled.p`
+    font-size: 0.8rem;
+    float: left;
+    margin-top: -0.2em;
+`;
+
+export const StyledGallRealtor1 = styled.p`
+    font-size: 0.8rem;
+    float: left;
+    margin-top: 4em;
+    margin-left: -9.6em;
+    color: blue;
+    font-weight: 500;
+`;
+
+export const StyledGallPrice2 = styled.h1`
+    font-weight: 500;
+    float: left;
+`;
+
+export const StyledGallAddr2 = styled.p`
+    font-size: 1.1rem;
+    float: left;
+    margin-top: -0.5em;
+`;
+
+export const StyledGallDes2 = styled.p`
+    font-size: 0.8rem;
+    float: left;
+    margin-top: -0.2em;
+`;
+
+export const StyledGallRealtor2 = styled.p`
+    font-size: 0.8rem;
+    float: left;
+    margin-top: 4em;
+    margin-left: -9.6em;
+    color: blue;
+    font-weight: 500;
+`;
+
+export const StyledGallPrice3 = styled.h1`
+    font-weight: 500;
+    float: left;
+`;
+
+export const StyledGallAddr3 = styled.p`
+    font-size: 1.1rem;
+    float: left;
+    margin-top: -0.5em;
+`;
+
+export const StyledGallDes3 = styled.p`
+    font-size: 0.8rem;
+    float: left;
+    margin-top: -0.2em;
+`;
+
+export const StyledGallRealtor3 = styled.p`
+    font-size: 0.8rem;
+    float: left;
+    margin-top: 4em;
+    margin-left: -9.6em;
+    color: blue;
+    font-weight: 500;
+`;
+
